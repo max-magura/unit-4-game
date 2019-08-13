75e479e661cc5a559146d3305653ef8b878f0edc
Max-M <maxnmagura@gmail.com>
2019-08-12 22:40:23 -0500

    added all html and css elements with IDs for all elements that will need to used in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  
+  var targetNumber = "";
+  var targetNumberArray = [];
+  var crystalNumberOptions = [];
+  var blueCrystalValue = "";
+  var orangeCrystalValue = "";
+  var greenCrystalValue = "";
+  var pinkCrystalValue = "";
+  var totalScore = "";
+  var gameWin = false;
+  var gameLoss = false;
+
+  $
+
+  function gameReset() {
+    targetNumber = "";
+    totalScore = "";
+    operator = "";
+
+    
+});
