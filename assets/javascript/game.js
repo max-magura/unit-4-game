3e9c041ae20e7acc75730882b0b89e5bcb68afec
Max-M <maxnmagura@gmail.com>
2019-08-12 23:58:57 -0500

    added in intial variables to js and began on first click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,15 +8,38 @@ $(document).ready(function() {
   var greenCrystalValue = "";
   var pinkCrystalValue = "";
   var totalScore = "";
-  var gameWin = false;
-  var gameLoss = false;
+  var totalScoreDisplayed = parseInt(totalScore);
 
-  $
+  for (var i = 19; i <= 200; i++) {
+    targetNumberArray.push(i);
+    console.log(targetNumberArray.length)
+  }
 
-  function gameReset() {
-    targetNumber = "";
-    totalScore = "";
-    operator = "";
+  for (var i = 1; i <= 12; i++) {
+    crystalNumberOptions.push(i);
+    console.log(crystalNumberOptions.length)
+  }
+
+    targetNumber = targetNumberArray[Math.floor(Math.random()*targetNumberArray.length)];
+    blueCrystalValue = crystalNumberOptions[Math.floor(Math.random()*crystalNumberOptions.length)];
+    orangeCrystalValue = crystalNumberOptions[Math.floor(Math.random()*crystalNumberOptions.length)];
+    greenCrystalValue = crystalNumberOptions[Math.floor(Math.random()*crystalNumberOptions.length)];
+    pinkCrystalValue = crystalNumberOptions[Math.floor(Math.random()*crystalNumberOptions.length)];
+
+    $("#blueCrystalValue").text(blueCrystalValue)
+    $("#orangeCrystalValue").text(orangeCrystalValue)
+    $("#greenCrystalValue").text(greenCrystalValue)
+    $("#pinkCrystalValue").text(pinkCrystalValue)
+    $("#random-number").text(targetNumber)
+    $("#total-score").text(totalScoreDisplayed)
+
+    $("#blue-crystal").on("click", function() {
+      blueCrystalValueInt = parseInt(blueCrystalValue)
+      totalScore += blueCrystalValueInt;
+      console.log(totalScore)
+    }); 
+    
+  
 
     
 });
