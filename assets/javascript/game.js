56379cf6f1209fb4593ddff8efd837b4343367ee
Max-M <maxnmagura@gmail.com>
2019-08-17 13:08:35 -0700

    added reset css, added js to show total score into correct div area, and added reset function to create rounds for the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
   var targetNumber = "";
   var blueCrystalValue;
   var orangeCrystalValue;
@@ -15,61 +15,90 @@ $(document).ready(function() {
   greenCrystalValue = Math.floor(Math.random()* (12 - 1 + 1)) + 1;
   pinkCrystalValue = Math.floor(Math.random()* (12 - 1 + 1)) + 1;
 
-  $("#random-number").text(targetNumber);
-  $("#total-score").text(totalScore);
-  $("win-number").text(wins);
-  $("loss-number").text(losses);
-
   var blueCrystalValueInt = parseInt(blueCrystalValue)
   var orangeCrystalValueInt = parseInt(orangeCrystalValue)
   var greenCrystalValueInt = parseInt(greenCrystalValue)
   var pinkCrystalValueInt = parseInt(pinkCrystalValue)
-  
+
+  $("#random-number").text(targetNumber);
+  $("#total-score").text(totalScore);
+  $("#win-number").text(wins);
+  $("#loss-number").text(losses);
 
     $("#blue-crystal").on("click", function clickCrystal() {
       totalScore += blueCrystalValueInt;
       $("#total-score").text(totalScore)
+      checkResults ()
     }) 
 
     $("#orange-crystal").on("click", function clickCrystal() {
       totalScore += orangeCrystalValueInt;
       $("#total-score").text(totalScore)
+      checkResults ()
     })
 
     $("#green-crystal").on("click", function clickCrystal() {
       totalScore += greenCrystalValueInt;
       $("#total-score").text(totalScore)
+      checkResults ()
     })
 
     $("#pink-crystal").on("click", function clickCrystal() {
       totalScore += pinkCrystalValueInt;
       $("#total-score").text(totalScore)
-      console.log(totalScore)
-
+      checkResults ()
     })
 
+    function checkResults () {
     if (totalScore === targetNumber) {
       wins++;
-      console.log(wins)
+      $("#win-number").text(wins);
+      alert("You win! :-)")
+      reset ()
     }
 
     if (totalScore > targetNumber) {
-      lossess++;
-      alert("You lose")
-    }
+      losses++;
+      $("#loss-number").text(losses);
+      alert("You lost :-(")
+      reset() 
+  }
+}
 
-    console.log(totalScore)
+function reset () {
+ totalScore = 0;
+
+ targetNumber = Math.floor(Math.random()* (200 - 19 + 1)) + 19;
+ blueCrystalValue = Math.floor(Math.random()* (12 - 1 + 1)) + 1;
+ orangeCrystalValue = Math.floor(Math.random()* (12 - 1 + 1)) + 1;;
+ greenCrystalValue = Math.floor(Math.random()* (12 - 1 + 1)) + 1;
+ pinkCrystalValue = Math.floor(Math.random()* (12 - 1 + 1)) + 1;
+
+  blueCrystalValueInt = parseInt(blueCrystalValue)
+  orangeCrystalValueInt = parseInt(orangeCrystalValue)
+  greenCrystalValueInt = parseInt(greenCrystalValue)
+  pinkCrystalValueInt = parseInt(pinkCrystalValue)
+
+  $("#random-number").text(targetNumber);
+  $("#total-score").text(totalScore);
+  $("#win-number").text(wins);
+  $("#loss-number").text(losses);
+}
 });
 
-
-         /*  
-           var crystalValueInt = [blueCrystalValueInt, orangeCrystalValueInt,greenCrystalValueInt, pinkCrystalValueInt]
+ /*  var crystalValueInt = [blueCrystalValueInt, orangeCrystalValueInt,greenCrystalValueInt, pinkCrystalValueInt]
          
-         $("#blue-crystal, #orange-crystal, #green-crystal, #pink-crystal").on("click", clickCrystal)
-    
-          function clickCrystal() {
-          for (var i=0; i< crystalValueInt.length; i++)
-          totalScore += crystalValueInt[i];
-          $("#total-score").text(totalScore);
-        }  
-        */
+  $("#blue-crystal, #orange-crystal, #green-crystal, #pink-crystal").on("click", clickCrystal)
+
+   function clickCrystal() {
+   for (var i=0; i< crystalValueInt.length; i++)
+   totalScore += crystalValueInt[i];
+   $("#total-score").text(totalScore);
+   console.log(crystalValueInt[i])
+   console.log(totalScore)
+ }  
+
+*/
+
+       
+  
